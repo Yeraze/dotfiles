405045d71c2fbed1930e30c0dac450fe5193aa32
Randall Hand <randall.hand@gmail.com>
2024-08-27 12:45:30 -0500

    Bugfix - for gunzip/bunzip

--- .config/fish/functions/expand.fish
+++ .config/fish/functions/expand.fish
@@ -13,16 +13,16 @@ function expand -d 'Decompress a file' -a filename
             pv $filename | tar jxf -
         case gz
             echo "Un-gz-ing $filename..."
-            pv $filename | gunzip -
+            pv $filename | gunzip - >(echo $filename | awk -v RS='\0' '{ sub(/\.gz$/, "") } 1')
         case bz
             echo "Un-bz-ing $filename..."
-            pv $filename | bunzip2 -
+            pv $filename | bunzip2 - >(echo $filename | awk -v RS='\0' '{ sub(/\.bz$/, "") } 1')
         case bz2
             echo "Un-bz-ing $filename..."
-            pv $filename | bunzip2 -
+            pv $filename | bunzip2 - >(echo $filename | awk -v RS='\0' '{ sub(/\.bz2$/, "") } 1')
         case zip
             echo "Un-zipping $filename..."
-            unzip $filename 
+            unzip $filename
         case '*'
             echo I don\'t know what to do with $extension files.
     end
