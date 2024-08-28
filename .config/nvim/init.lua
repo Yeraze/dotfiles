3b989e3b93af5316168e0ee0de303a765bf19121
Randall Hand <randall.hand@gmail.com>
2024-08-28 15:18:04 -0500

    Added neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+require("lspconfig").pyright.setup({})
+require("lspconfig").eslint.setup({})
+require("lspconfig").sourcekit.setup({})
+require("lspconfig").clangd.setup({})
+require("mini.surround").setup({
+  highlight_duration = 5000,
+})
