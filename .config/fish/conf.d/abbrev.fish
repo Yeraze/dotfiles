c965d648fbe855d74d79e73e9989d2e3f0ed183c
Randall Hand <randall.hand@gmail.com>
2024-09-16 09:32:04 -0500

    Added dir-first to ll abbrev

--- .config/fish/conf.d/abbrev.fish
+++ .config/fish/conf.d/abbrev.fish
@@ -5,6 +5,6 @@ abbr -a !! --position anywhere --function last_history_item
 
 abbr -a vi nvim
 abbr -a vim nvim
-abbr -a ll "eza -la --header --icons --no-user --git $argv"
+abbr -a ll "eza -la --header --icons --no-user --git --group-directories-first $argv"
 abbr -a cat bat
 abbr -a cd z
