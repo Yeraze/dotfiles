4f60f4d7b8bfe459c7e106fa316ec8afdd46627d
Randall Hand <randall.hand@gmail.com>
2024-09-16 09:45:55 -0500

    Added lll abbrev

--- .config/fish/conf.d/abbrev.fish
+++ .config/fish/conf.d/abbrev.fish
@@ -6,5 +6,6 @@ abbr -a !! --position anywhere --function last_history_item
 abbr -a vi nvim
 abbr -a vim nvim
 abbr -a ll "eza -la --header --icons --no-user --git --group-directories-first $argv"
+abbr -a lll "eza -la --header --icons --no-user --git --group-directories-first --git-repos $argv"
 abbr -a cat bat
 abbr -a cd z
