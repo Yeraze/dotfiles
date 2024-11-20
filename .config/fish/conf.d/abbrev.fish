ffd96531e211b60458bd0481254e323740913511
Randall Hand <randall.hand@gmail.com>
2024-11-20 11:56:15 -0600

    Add -M to eza abbreviation

--- .config/fish/conf.d/abbrev.fish
+++ .config/fish/conf.d/abbrev.fish
@@ -5,7 +5,7 @@ abbr -a !! --position anywhere --function last_history_item
 
 abbr -a vi nvim
 abbr -a vim nvim
-abbr -a ll "eza -la --header --icons --no-user --git --group-directories-first $argv"
+abbr -a ll "eza -la --header --icons --no-user --git --group-directories-first -M $argv"
 abbr -a lll "eza -la --header --icons --no-user --git --group-directories-first --git-repos $argv"
 abbr -a cat bat
 abbr -a cd z
