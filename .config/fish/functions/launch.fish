7774fb147396bf18148fb06914293bf3540e870c
Randall Hand <randall.hand@gmail.com>
2024-12-25 08:10:54 -0500

    Updated launch.fish to support different SSH's

--- .config/fish/functions/launch.fish
+++ .config/fish/functions/launch.fish
@@ -1,5 +1,11 @@
 # nvm
 function launch --description "Launch SSH to a newly named tmux window"
     tmux renamew $argv
-    /usr/local/ossh/bin/ssh $argv
+    if string match -q "*hpc.mil*" $argv
+        echo Using Kerberized SSH
+        /usr/local/ossh/bin/ssh $argv
+    else
+        echo Using Vendor SSH
+        /usr/bin/ssh $argv
+    end
 end
