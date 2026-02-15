7ed61da35c719ba08bd018a5586edc5a7e5969fe
Randall Hand <randall.hand@gmail.com>
2026-02-15 17:07:45 -0500

    Updates to lots of things

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ set -x PATH ~/Library/Python/2.7/bin $PATH
 
 thefuck --alias | source
 
-pyenv init - fish | source
+#pyenv init - fish | source
 fish_add_path /usr/local/opt/openjdk/bin
 
 oh-my-posh init fish --config ~/.config/yeraze.omp.json | source
@@ -14,4 +14,9 @@ zoxide init fish | source
 
 if status --is-login
     SHELL=fish EDITOR=nvim PF_INFO="ascii title os host kernel uptime pkgs memory shell editor" pfetch
+end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/opt/homebrew/share/google-cloud-sdk/path.fish.inc' ]
+    . '/opt/homebrew/share/google-cloud-sdk/path.fish.inc'
 end
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("lspconfig").pyright.setup({})
-require("lspconfig").eslint.setup({})
-require("lspconfig").sourcekit.setup({})
-require("lspconfig").clangd.setup({})
-require("lspconfig").perlpls.setup({})
-
-require("mini.surround").setup({
-  highlight_duration = 5000,
-})
